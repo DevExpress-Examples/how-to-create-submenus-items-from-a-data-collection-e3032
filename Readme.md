8aa943a53c30e85dfd6feb793be965deddd489ed
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create SubMenu's items from a data collection
+
+
+<p>This example demonstrates how to populate the menu based upon a specific data source using a data-binding mechanism.<br /><br />To learn more on how to implement similar functionality in <strong>Silverlight</strong>, refer to the <a href="https://www.devexpress.com/Support/Center/p/T245564">T245564</a> example.</p>
+
+<br/>
+
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Bars
+Imports System.Collections.ObjectModel
+Imports System.Collections
+Imports System.Collections.Specialized
+
+Namespace Q158708
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
